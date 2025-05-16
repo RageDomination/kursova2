08722ac4fb4b69cf614b2dcdffae400501c96f32
Владос царь / <154845500+RageDomination@users.noreply.github.com>
2025-05-16 17:09:32 +0300

    Готова візуальна форма кошика

--- курсова2/Form4.Designer.cs
+++ курсова2/Form4.Designer.cs
@@ -47,6 +47,9 @@
             button3 = new Button();
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
@@ -66,7 +69,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(423, 422);
+            label2.Location = new Point(82, 422);
             label2.Name = "label2";
             label2.Size = new Size(61, 21);
             label2.TabIndex = 2;
@@ -120,7 +123,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(632, 96);
+            label3.Location = new Point(632, 83);
             label3.Name = "label3";
             label3.Size = new Size(242, 25);
             label3.TabIndex = 13;
@@ -128,21 +131,21 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(661, 166);
+            textBox1.Location = new Point(661, 136);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(202, 23);
             textBox1.TabIndex = 14;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(661, 215);
+            textBox2.Location = new Point(661, 185);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(202, 23);
             textBox2.TabIndex = 15;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(661, 266);
+            textBox3.Location = new Point(661, 285);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(202, 23);
             textBox3.TabIndex = 16;
@@ -150,7 +153,7 @@
             // dateTimePicker1
             // 
             dateTimePicker1.Format = DateTimePickerFormat.Custom;
-            dateTimePicker1.Location = new Point(661, 312);
+            dateTimePicker1.Location = new Point(661, 335);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.RightToLeft = RightToLeft.Yes;
             dateTimePicker1.Size = new Size(202, 23);
@@ -161,7 +164,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(716, 143);
+            label4.Location = new Point(716, 113);
             label4.Name = "label4";
             label4.Size = new Size(94, 21);
             label4.TabIndex = 18;
@@ -171,7 +174,7 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(698, 192);
+            label5.Location = new Point(698, 162);
             label5.Name = "label5";
             label5.Size = new Size(134, 21);
             label5.TabIndex = 19;
@@ -181,17 +184,17 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.Location = new Point(674, 242);
+            label6.Location = new Point(698, 261);
             label6.Name = "label6";
-            label6.Size = new Size(189, 21);
+            label6.Size = new Size(138, 21);
             label6.TabIndex = 20;
-            label6.Text = "Введіть адресу доставки ";
+            label6.Text = " Адреса доставки ";
             // 
             // button3
             // 
-            button3.Location = new Point(674, 353);
+            button3.Location = new Point(676, 408);
             button3.Name = "button3";
-            button3.Size = new Size(176, 35);
+            button3.Size = new Size(169, 35);
             button3.TabIndex = 21;
             button3.Text = "Зробити замовлення";
             button3.UseVisualStyleBackColor = true;
@@ -199,9 +202,9 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(567, 411);
+            pictureBox1.Location = new Point(685, 364);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(65, 44);
+            pictureBox1.Size = new Size(75, 38);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 22;
             pictureBox1.TabStop = false;
@@ -209,18 +212,48 @@
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(638, 411);
+            pictureBox2.Location = new Point(758, 364);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(65, 44);
+            pictureBox2.Size = new Size(74, 38);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 23;
             pictureBox2.TabStop = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(661, 235);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(202, 23);
+            textBox4.TabIndex = 24;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(702, 211);
+            label7.Name = "label7";
+            label7.Size = new Size(130, 21);
+            label7.TabIndex = 25;
+            label7.Text = "Номер телефону";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(685, 311);
+            label8.Name = "label8";
+            label8.Size = new Size(160, 21);
+            label8.TabIndex = 26;
+            label8.Text = "Дата та час доставки";
             // 
             // Form4
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(886, 463);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(textBox4);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Controls.Add(button3);
@@ -265,5 +298,8 @@
         private Button button3;
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
+        private TextBox textBox4;
+        private Label label7;
+        private Label label8;
     }
 }
